e38d9f57dc5b14141bee862153fae0017a1563d9
dawson-lefebvre <112987023+dawson-lefebvre@users.noreply.github.com>
2024-04-19 11:08:44 -0600

    Commented question 1

--- Assets/Question1.cs
+++ Assets/Question1.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Question1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //This one is tricky. I'll come back to it if I have the time.
+
+    int Func(int[] arr, int a, int b, int c)
     {
-        
+        int d = (b + c) / 2;
+
+        if (b > c)
+        {
+            return -1;
+        }
+
+        if (arr[d] < a)
+        {
+            return Func(arr, a, d + 1, c);
+        }
+        else if (arr[d] > a)
+        {
+            return Func(arr, a, b, d - 1);
+        }
+        else
+        {
+            return d;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
